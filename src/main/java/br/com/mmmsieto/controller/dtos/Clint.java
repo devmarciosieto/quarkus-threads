9e923d2467dc95870b5eb49cb1fa8e52935b8c3f
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2024-01-23 23:58:02 -0300

    update

--- src/main/java/br/com/mmmsieto/controller/dtos/Clint.java
+++
@@ -1,9 +0,0 @@
-package br.com.mmmsieto.controller.dtos;
-
-import java.util.UUID;
-
-public record Clint(UUID id, String name, Address address) {
-    public Clint {
-        id = UUID.randomUUID();
-    }
-}
--- src/main/java/br/com/mmmsieto/controller/dtos/ClintDeserializer.java
+++
@@ -1,9 +0,0 @@
-package br.com.mmmsieto.controller.dtos;
-
-import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
-
-public class ClintDeserializer extends ObjectMapperDeserializer<Clint> {
-    public ClintDeserializer() {
-        super(Clint.class);
-    }
-}
